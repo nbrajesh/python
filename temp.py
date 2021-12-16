717fa5d55790242ac64c0857b11bb9e2fc0dc55f
Rajesh <83630991+nbrajesh@users.noreply.github.com>
2021-12-16 20:44:55 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,12 +1,20 @@
-from PyPDF2 import PdfFileMerger
-
-pdfs = ['file1.pdf', 'file2.pdf', 'file3.pdf']
-
+from pathlib import Path # Standard Python Library
+import xlwings as xw # pip install xlwings
+from PyPDF2 import PdfFileMerger, PdfFileReader # pip install PyPDF2
+def main():
+wb = xw.Book.caller()
+sheet = wb.sheets[0]
 merger = PdfFileMerger()
+sheet.range(&quot;status&quot;).clear_contents()
+source_dir = sheet.range(&quot;source_dir&quot;).value
+output_name = sheet.range(&quot;output_name&quot;).value + &quot;.pdf&quot;
+pdf_files = list(Path(source_dir).glob(&quot;*.pdf&quot;))
+for pdf_file in pdf_files:
+merger.append(PdfFileReader(str(pdf_file), &quot;rb&quot;))
 
-for pdf in pdfs:
-    merger.append(pdf)
-
-merger.write("result.pdf")
-print("Successfully Merged All PDF files")
-merger.close()
+output_path = str(Path(__file__).parent / output_name)
+merger.write(output_path)
+sheet.range(&quot;status&quot;).value = f&quot;The file have been saved here: {output_path}&quot;
+if __name__ == &quot;__main__&quot;:
+xw.Book(&quot;pdfmerger.xlsm&quot;).set_mock_caller()
+main()
